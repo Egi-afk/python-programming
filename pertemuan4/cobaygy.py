cf17eaab31e483fd887ce851ef75aabd7d789ba1
Egi-afk <ergiano900@gmail.com>
2024-07-31 22:12:29 -0700

    Add files via upload

---
+++ pertemuan4/cobaygy.py
@@ -0,0 +1,5 @@
+#Menyatukan Dua Variabel 
+flower = str(input("Apa bunga favoritemu?"))
+pet = str(input("Apa hewan yang kamu sukai?"))
+gabung = flower + '' +pet
+print("Gabungannya katanya adalah:{gabung}")
