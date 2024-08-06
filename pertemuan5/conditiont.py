aee1c20be0fda956a13c0d904e065483c448150b
Egi-afk <ergiano900@gmail.com>
2024-08-05 19:41:17 -0700

    Add files via upload

---
+++ pertemuan5/conditiont.py
@@ -0,0 +1,9 @@
+x = 2 
+
+if x % 2 == 0:
+    print(x,"adalah bilangan genap")
+else:
+    print(x,"adalah bilangan ganjil")
+
+
+
---
+++ pertemuan5/membership.py
@@ -0,0 +1,7 @@
+makanan = ["kue pancong","nasi goreng","salad","lotek"]
+
+print("Apakah nasi uduk merupakan anggota","nasi uduk" in makanan)
+print("apakah salad merupakan anggota","salad" in makanan)
+
+print("Apakah nasi uduk merupakan anggota","nasi uduk" not in makanan)
+print("apakah salad merupakan anggota","salad" not in makanan)
