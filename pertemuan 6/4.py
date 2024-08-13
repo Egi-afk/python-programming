8c3d6fd2887e0253dd57504fddacfec08b42fb38
Egi-afk <ergiano900@gmail.com>
2024-08-12 19:53:05 -0700

    Add files via upload

---
+++ pertemuan 6/4.py
@@ -0,0 +1,13 @@
+x = 10
+y = 5 
+z = 7 
+
+if x>y and x>z:
+    print ("x lebih besar")
+elif y>x and y>z:
+    print("y leboih besar")
+elif z>x and z>y:
+    print ("z lebih besar")
+else:
+    print("print tidak ada yang benar")
+    
